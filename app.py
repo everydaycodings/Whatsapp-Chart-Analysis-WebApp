5b8ac0efde3d6253a11d8d9a3fc0b9cc0bf507cb
Kumar Saksham <everydaycodings@gmail.com>
2021-12-01 20:13:22 +0530

    app.py - added upload button and printed the result

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.sidebar.title("Hello World")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    st.text(data)

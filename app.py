04f81982a22f5af7a0cf95ed361da9d74bb84d62
Kumar Saksham <everydaycodings@gmail.com>
2021-12-01 20:44:44 +0530

    app.py - added dropdown menu for user analysis

--- app.py
+++ app.py
@@ -10,3 +10,11 @@ if uploaded_file is not None:
     data = bytes_data.decode("utf-8")
     data = preprocessor.preprocess(data)
     st.dataframe(data)
+
+    user_list = data["user"].unique().tolist()
+    #st.sidebar.selectbox("Show Analysis with Respect to {}".format(user_list))
+
+    option = st.sidebar.selectbox('Which user would you like to see Analysis',
+      (user_list))
+
+    st.write('Show Analysis with Respect to', option)

f2d6b1b2a7f4738ddf30a1b72ab1348f50fef76d
Kumar Saksham <everydaycodings@gmail.com>
2021-12-01 21:25:22 +0530

    helper.py - restructured the code

--- helper.py
+++ helper.py
@@ -1,21 +1,13 @@
 def fetch_stats(selected_data, data):
-    if selected_data == "Overall":
+    if selected_data != "Overall":
 
-        words = []
-        for message in data["message"]:
-            words.extend(message.split())
+        data = data[data["user"] == selected_data]
 
-        num_messages = data.shape[0]
-
-        return num_messages, len(words)
+    num_messages = data.shape[0]
     
-    else:
-        selected_data = data[data["user"] == selected_data]
+    words = []
+    for message in data["message"]:
+        words.extend(message.split())
 
-        num_messages = selected_data.shape[0]
-        
-        words = []
-        for message in selected_data["message"]:
-            words.extend(message.split())
-        
-        return num_messages, len(words)
+    return num_messages, len(words)
+    

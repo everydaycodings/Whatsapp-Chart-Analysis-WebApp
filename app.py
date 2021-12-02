42415bddd2d839cc67ecead6d9044050553978b0
Kumar Saksham <everydaycodings@gmail.com>
2021-12-02 19:58:10 +0530

    add download buton for downloading sample data and cleaned CSV File

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import streamlit as st
 import preprocessor, helper
 import matplotlib.pyplot as plt
+import pandas as pd
 
 st.sidebar.title("Hello World")
 
+sample_data = pd.read_csv("data/sample.csv")
+
+st.sidebar.download_button(
+        label="Download Sample CSV Data and Use It",
+        data=sample_data.to_csv(),
+        file_name='whatsapp_smaple_data.csv',
+        mime='text/csv',
+        help = "When You Click On Download Button You WhatsApp Text Data Will Be Converted To Clean CSV File"
+    )
+
+
 uploaded_file = st.sidebar.file_uploader("Choose a file")
+
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
@@ -12,15 +25,24 @@ if uploaded_file is not None:
     data = preprocessor.preprocess(data)
     st.dataframe(data)
 
+    st.download_button(
+        label="Download data as CSV",
+        data=data.to_csv(),
+        file_name='whatsapp_data_output.csv',
+        mime='text/csv',
+        help = "When You Click On Download Button You WhatsApp Text Data Will Be Converted To Clean CSV File"
+    )
+
     user_list = data["user"].unique().tolist()
     user_list.sort()
     user_list.remove("group_notification")
     user_list.insert(0, "Overall")
-    selected_user = st.sidebar.selectbox('Which user would you like to see Analysis',(user_list))
+    selected_user = st.sidebar.selectbox('Which user would you like to see Analysis(Select and Click Show Analysis)',(user_list))
 
     if st.sidebar.button("Show Analysis"):
         num_messages, words, num_media_messages, link = helper.fetch_stats(selected_user, data)
 
+        st.title("Top Statistics")
         col1, col2, col3, col4 = st.columns(4)
 
         with col1:
